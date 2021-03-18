45a264f38259401c6f0d64495370d26c86e91f47
Andrii Frunt <andrey.frunt@gmail.com>
2021-03-18 13:45:59 +0200

    init

--- src/main/java/com/afrunt/h2s/H2Snapshot.java
+++ src/main/java/com/afrunt/h2s/H2Snapshot.java
@@ -1,15 +1,36 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
 package com.afrunt.h2s;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.sql.DataSource;
+import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.sql.Connection;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.Objects;
+import java.util.UUID;
 
 /**
  * @author Andrii Frunt
@@ -21,8 +42,8 @@ public class H2Snapshot {
     private final Path dumpFilePath;
 
     public H2Snapshot(DataSource dataSource) {
-        new H2DataSourceValidator().validate(dataSource);
-        this.dirPath = Utils.createRandomTempDirectoryWithAutoDeletion();
+        validateDataSource(dataSource);
+        this.dirPath = createRandomTempDirectoryWithAutoDeletion();
         this.dumpFilePath = dirPath.resolve("dump.sql").toAbsolutePath();
         final long started = System.currentTimeMillis();
         saveDump(dataSource);
@@ -63,6 +84,66 @@ public class H2Snapshot {
             stat.close();
         } catch (SQLException e) {
             throw new RuntimeException(e);
+        }
+    }
+
+    private void validateDataSource(DataSource dataSource) {
+        Objects.requireNonNull(dataSource);
+
+        try (var connection = dataSource.getConnection()) {
+            String databaseProductName = connection.getMetaData().getDatabaseProductName();
+            if (!"H2".equalsIgnoreCase(databaseProductName)) {
+                throw new IllegalArgumentException("Unknown type of the database " + databaseProductName);
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private boolean deleteDirectory(Path path) {
+        return deleteDirectory(path.toFile());
+    }
+
+    private boolean deleteDirectory(File directoryToBeDeleted) {
+        File[] allContents = directoryToBeDeleted.listFiles();
+        if (allContents != null) {
+            for (File file : allContents) {
+                deleteDirectory(file);
+            }
+        }
+        return directoryToBeDeleted.delete();
+    }
+
+    private Path tempDirectoryPath() {
+        return Path.of(System.getProperty("java.io.tmpdir"));
+    }
+
+    private Path createRandomTempDirectoryWithAutoDeletion() {
+        return createRandomTempDirectory(true);
+    }
+
+    private Path createRandomTempDirectory(boolean autoDeletion) {
+        Path tempDirectoryPath = tempDirectoryPath();
+
+        Path dirPath;
+
+        do {
+            dirPath = tempDirectoryPath.resolve(UUID.randomUUID().toString());
+        } while (dirPath.toFile().exists());
+
+        try {
+            Files.createDirectories(dirPath);
+            if (autoDeletion) {
+                final Path pathToDelete = dirPath;
+                Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+                            deleteDirectory(pathToDelete);
+                            LOGGER.debug("Temporary directory {} deleted", pathToDelete.toAbsolutePath());
+                        })
+                );
+            }
+            return dirPath.toAbsolutePath();
+        } catch (IOException e) {
+            throw new RuntimeException("Failed to create directory " + dirPath.toAbsolutePath(), e);
         }
     }
 }
--- src/test/java/com/afrunt/test/h2s/H2SnapshotTest.java
+++ src/test/java/com/afrunt/test/h2s/H2SnapshotTest.java
@@ -1,27 +1,80 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
 package com.afrunt.test.h2s;
 
 import com.afrunt.h2s.H2Snapshot;
+import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.jdbc.core.JdbcTemplate;
 
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.DatabaseMetaData;
+import java.sql.SQLException;
+
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 /**
  * @author Andrii Frunt
  */
 public class H2SnapshotTest {
+
+    public static HikariDataSource createInMemoryDataSource() {
+        HikariConfig config = new HikariConfig();
+        config.setJdbcUrl("jdbc:h2:mem:myDatabase;DB_CLOSE_DELAY=-1;LOG=0");
+
+        return new HikariDataSource(config);
+    }
+
+    @Test
+    public void testNullOriginalDataSource() {
+        Assertions.assertThatThrownBy(() -> new H2Snapshot(null)).isExactlyInstanceOf(NullPointerException.class);
+    }
+
+    @Test
+    public void testIncorrectTypeOfDataBase() throws SQLException {
+        DataSource dataSource = mock(DataSource.class);
+        Connection connection = mock(Connection.class);
+        DatabaseMetaData databaseMetaData = mock(DatabaseMetaData.class);
+        when(dataSource.getConnection()).thenReturn(connection);
+        when(connection.getMetaData()).thenReturn(databaseMetaData);
+        when(databaseMetaData.getDatabaseProductName()).thenReturn("DEAD BEAF");
+
+        Assertions.assertThatThrownBy(() -> new H2Snapshot(dataSource)).isExactlyInstanceOf(IllegalArgumentException.class);
+    }
+
+
     @Test
     public void testSnapshotCreation() {
-        final H2Snapshot h2Snapshot = new H2Snapshot(H2Utils.createInMemoryDataSource());
+        final H2Snapshot h2Snapshot = new H2Snapshot(createInMemoryDataSource());
         assertThat(h2Snapshot.getDirPath().toFile().exists()).isTrue();
         assertThat(h2Snapshot.getDumpFilePath().toFile().exists()).isTrue();
     }
 
     @Test
     public void testRestoreFromSnapshot() {
-        final HikariDataSource dataSource = H2Utils.createInMemoryDataSource();
+        final HikariDataSource dataSource = createInMemoryDataSource();
         final H2Snapshot initialStateSnapshot = new H2Snapshot(dataSource);
 
         System.out.println(initialStateSnapshot.getDumpFileContents());
@@ -48,8 +101,8 @@ public class H2SnapshotTest {
     }
 
     @Test
-    public void testDeletedDumpFile(){
-        final HikariDataSource dataSource = H2Utils.createInMemoryDataSource();
+    public void testDeletedDumpFile() {
+        final HikariDataSource dataSource = createInMemoryDataSource();
         final H2Snapshot initialStateSnapshot = new H2Snapshot(dataSource);
         final boolean dumpFileDeleted = initialStateSnapshot.getDumpFilePath().toFile().delete();
         assertThat(dumpFileDeleted).isTrue();
